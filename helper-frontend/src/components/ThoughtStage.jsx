c046a914736d288fd1f6f3c78279172b766fb4c6
Roseline <144480191+Roseline-Projects@users.noreply.github.com>
2024-10-08 20:22:25 -0400

    Update ThoughtStage.jsx

--- helper-frontend/src/components/ThoughtStage.jsx
+++ helper-frontend/src/components/ThoughtStage.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "./thoughtstage.css"
-import CytoscapeComponent from 'react-cytoscapejs'
 import { useEffect, useRef} from "react";
-import { Network } from 'vis-network'
 import ThoughtNetwork from "./ThoughtNetwork";
 
 
